59028f0714a27d29569978709bfa4edbf09a2f41
ysy <1156546473@qq.com>
2019-03-15 22:27:06 +0800

    use widget folder

--- lib/src/view/wipe_out.dart
+++ lib/src/view/wipe_out.dart
@@ -4,7 +4,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
-import '../../widget.dart';
+import '../../widget/RadioTile.dart';
 
 enum SingingCharacter { lafayette, jefferson }
 
